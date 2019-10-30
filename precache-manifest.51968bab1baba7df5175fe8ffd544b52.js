aeda7348575be208c82269614d9a64ed754a0d7c
Shijun  Yu <yus3@rose-hulman.edu>
2019-10-30 01:16:38 -0400

    Updates

---
+++ precache-manifest.51968bab1baba7df5175fe8ffd544b52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "689bdc428ddd84b1baeb5c401c69b8b0",
+    "url": "/samantha-design-project/index.html"
+  },
+  {
+    "revision": "b6624a5be782d09b18fc",
+    "url": "/samantha-design-project/static/css/main.aa0c08a5.chunk.css"
+  },
+  {
+    "revision": "a94b37dedae0700d773f",
+    "url": "/samantha-design-project/static/js/2.4d3ca9c9.chunk.js"
+  },
+  {
+    "revision": "b6624a5be782d09b18fc",
+    "url": "/samantha-design-project/static/js/main.f224490f.chunk.js"
+  },
+  {
+    "revision": "ee3a7a0d61595bd710fe",
+    "url": "/samantha-design-project/static/js/runtime-main.ebe9e9fb.js"
+  }
+]);
